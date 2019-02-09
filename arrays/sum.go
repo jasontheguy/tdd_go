c91e62234c2dbf42f5936285319b742cc5455f18
Jason Renaud <renaudjj@g.cofc.edu>
2019-02-09 10:33:00 -0700

    Committing before reimaging OS

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,21 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, num := range numbers {
 		sum += num
 	}
 	return sum
+}
+
+//...[]int is variadic syntax. Can take variable num of args
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNums := len(numbersToSum)
+
+	sums := make([]int, lengthOfNums)
+
+	for i, nums := range numbersToSum {
+		sums[i] = Sum(nums)
+	}
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,13 +1,27 @@
 package main
 
 import "testing"
+import "reflect"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if want != got {
-		t.Errorf("Got %d want %d given, %v", got, want, numbers)
+	//Arrays are fixed, slices are dynamic
+	t.Run("collection of 5 integers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if want != got {
+			t.Errorf("Got %d want %d given, %v", got, want, numbers)
 
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
+
 }
