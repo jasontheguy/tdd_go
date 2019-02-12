f4ba3b83a40894e6f99eed1c46c4529a76a3800a
Jason Renaud <renaudjj@g.cofc.edu>
2019-02-12 09:59:43 -0700

    Implementing interfaces next

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -3,5 +3,30 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
 
+func TestArea(t *testing.T) {
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.16
+		if got != want { //Erroring out? It's a float, have to check for casting possible to pass
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
